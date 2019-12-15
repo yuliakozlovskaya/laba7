3e09a6e268726024f04fbc3a05e4843c1bb4586e
yuliakozlovskaya <49129572+yuliakozlovskaya@users.noreply.github.com>
2019-12-15 23:45:06 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,54 @@
+public class Car implements Printable {
+    String brand;
+    String model;
+    String configuration;
+    int price;
+    int year;
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getConfiguration() {
+        return configuration;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setConfiguration(String configuration) {
+        this.configuration = configuration;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public Car(String brand, String model, String configuration, int price, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.configuration = configuration;
+        this.price = price;
+        this.year = year;
+    }
+    @Override
+    public void println() {
+        System.out.println("Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", configuration='" + configuration + '\'' +
+                ", price=" + price +
+                ", year=" + year +
+                '}');
+    }
+}
---
+++ TestCar.java
@@ -0,0 +1,10 @@
+public class TestCar {
+    public static void main(String[] args) {
+        Car c1 = new Car("Audi", "rs6", "Sport", 8000000, 2018);
+        Car c2 = new Car("Ford", "Focus", "Standart", 500000, 2012);
+        Car c3 = new Car("Fiat", "H9", "Standart", 2300000, 2010);
+        c1.println();
+        c2.println();
+        c3.println();
+    }
+}
